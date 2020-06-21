58eefd112eab0edfa2607f4cb12c051996455f1f
Tomoyuki Sakurai <y@trombik.org>
2020-06-21 21:10:29 +0700

    initial implementation

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -f avrdude
+rm -f package.json
---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+: ${PREFIX:="/usr/local"}
+
+VERSION="1.60300.200527"
+DESC="tool-avrdude"
+REPO_NAME="platformio-freebsd-${DESC}"
+REPO_OWNER="trombik"
+
+ARCH=`uname -p`
+OS=`uname -s | tr '[[:upper:]]' '[[:lower:]]'`
+
+if [ ! -z $1 ]; then
+    VERSION="$1"
+fi
+
+ln -s -f ${PREFIX}/bin/avrdude .
+
+cat <<__EOF__ >package.json
+{
+    "description": "${DESC}",
+    "name": "${DESC}",
+    "system": [
+        "${OS}_${ARCH}"
+    ],
+    "url": "https://github.com/${REPO_OWNER}/${REPO_NAME}",
+    "version": "${VERSION}"
+}
+__EOF__
